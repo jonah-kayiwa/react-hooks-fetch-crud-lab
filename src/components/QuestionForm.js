06c550972aae22c90ebfb346f353c4a81af6e1e5
jonah-kayiwa <152336281+jonah-kayiwa@users.noreply.github.com>
2024-02-07 11:52:51 +0300

    done

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -19,8 +19,39 @@ function QuestionForm(props) {
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(formData);
+  
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        prompt: formData.prompt,
+        answers: [
+          formData.answer1,
+          formData.answer2,
+          formData.answer3,
+          formData.answer4,
+        ],
+        correctIndex: formData.correctIndex,
+      })
+    };
+  
+    fetch('http://localhost:4000/questions', requestOptions)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        props.onAddQuestion(data);
+      });
+  
+    setFormData({
+      prompt: '',
+      answer1: '',
+      answer2: '',
+      answer3: '',
+      answer4: '',
+      correctIndex: 0,
+    });
   }
+  
 
   return (
     <section>
